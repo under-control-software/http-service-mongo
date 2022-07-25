f6cea1c675685481993965247d88058f7df4eb7b
Divyansh <divyansh.24888@gmail.com>
2022-07-25 12:11:27 +0530

    Refactoring code

--- controllers/mongo-controllers.js
+++ controllers/mongo-controllers.js
@@ -1,14 +1,14 @@
 const axios = require('axios');
 const serverUrl = "https://sweet-ladybug-42.telebit.io/api/mongo";
-
+const test = false;
 const handleQuery = async (req, res, next) => {
     // console.log("req :", req);
     try {
-        console.log("request :", req.body);
+        test && console.log("request :", req.body);
         const requestQuery = req.body;
-        console.log("requestQuery :", requestQuery);
+        test && console.log("requestQuery :", requestQuery);
         const response = await axios.post(serverUrl, requestQuery);
-        console.log("response :", response);
+        test && console.log("response :", response);
         res.status(200).json(response.data);
     } catch (error) {
         console.log("error :", error);
